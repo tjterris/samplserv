24785a768707facda26ef46ebfb51bb7e2b18481
TuckerWatts <tucker@tuckerwatts.com>
2015-06-03 12:47:24 -0400

    added my version of client.rb and samplserv.rb"

--- lib/client.rb
+++ lib/client.rb
@@ -5,12 +5,100 @@ class SamplParty
   include HTTParty
   base_uri "http://localhost:4567"
 
-  def play_harder(version=nil)
+  def get_options(version=nil)
     options = {}
     if version
       options = { query: { v: version } }
     end
+    options
+  end
+
+  def play_sample(sample, version=nil)
+    self.class.post("/#{sample}", options(version))
+  end
+
+  def song(sample, version=nil)
+    toy.play_sample("/harder", 1)
+  end
+
+  def play_after(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/after", options)
+  end
+
+  def play_better(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/better", options)
+  end
+
+  def play_doit(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/doit", options)
+  end
+
+  def play_ever(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/ever", options)
+  end
+
+  def play_faster(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/faster", options)
+  end
+
+  def play_harder(verson=nil)
+    options = self.get_options(verson)
     self.class.post("/harder", options)
+  end
+
+  def play_hour(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/hour", options)
+  end
+
+  def play_makeit(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/makeit", options)
+  end
+
+  def play_makesus(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/makesus", options)
+  end
+
+  def play_morethan(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/morethan", options)
+  end
+
+  def play_never(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/never", options)
+  end
+
+  def play_our(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/our", options)
+  end
+
+  def play_over(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/over", options)
+  end
+
+  def play_stronger(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/stronger", options)
+  end
+
+  def play_workis(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/workis", options)
+  end
+
+  def play_workit(verson=nil)
+    options = self.get_options(verson)
+    self.class.post("/workit", options)
   end
 
   def play_beat
@@ -19,17 +107,13 @@ class SamplParty
 end
 
 toy = SamplParty.new
-toy.play_beat
+toy.play_harder
 sleep 5
-toy.play_harder(2)
-sleep 0.5
 toy.play_harder
-sleep 1
-toy.play_harder
+sleep 3
 toy.play_harder(2)
-sleep 1
-toy.play_beat
-sleep 1
 toy.play_beat
 
+
 puts "Cool cool cool ..."
+
--- lib/samplserv.rb
+++ lib/samplserv.rb
@@ -6,15 +6,85 @@ module Samplserv
   # Your code goes here...
 end
 
-post '/beat' do
-  spawn("afplay samples/beat.mp3")
+
+helpers do
+  def play_sample(sample)
+    v = params['v'] ? params['v'].to_i : 1
+    spawn("afplay samples/#{sample}#{v}.mp3")
+  end
 end
 
 get '/stop' do
   spawn("killall afplay")
 end
 
+post '/after' do
+  play_sample("After")
+end
+
+post '/better' do
+  play_sample("Better")
+end
+
+post '/doit' do
+  play_sample("DoIt")
+end
+
+post '/ever' do
+  play_sample("Ever")
+end
+
+post '/faster' do
+  play_sample("Faster")
+end
+
 post '/harder' do
-  v = params['v'] ? params['v'].to_i : 1
-  spawn("afplay samples/Harder#{v}.mp3")
+  play_sample("Harder")
+end
+
+post '/hour' do
+  play_sample("Hour")
+end
+
+post '/makeit' do
+  play_sample("MakeIt")
+end
+
+
+post '/morethan' do
+  play_sample("MoreThan")
+end
+
+
+post '/never' do
+  play_sample("Never")
+end
+
+
+post '/our' do
+  play_sample("Our")
+end
+
+
+post '/over' do
+  play_sample("Over")
+end
+
+
+post '/stronger' do
+  play_sample("Stronger")
+end
+
+
+post '/workis' do
+  play_sample("WorkIs")
+end
+
+
+post '/workit' do
+  play_sample("WorkIt")
+end
+
+post '/beat' do
+  spawn("afplay samples/beat.mp3")
 end
